109cebd43846c086fbd6a3d3fa6c0d7c7f2e593d
kunals131 <86314140+kunals131@users.noreply.github.com>
2021-09-21 02:39:08 +0530

    Loader

--- dist/js/app.js
+++ dist/js/app.js
@@ -15,29 +15,39 @@ function makeitbold(selected) {
     document.querySelector(`#about`).style.color = inactive_color;
     document.querySelector(`#work`).style.color = inactive_color;
     document.querySelector(`#more`).style.color = inactive_color;
-    document.querySelector('#about').style.fontWeight = 500;
-    document.querySelector('#work').style.fontWeight = 500;
-    document.querySelector('#more').style.fontWeight = 500;
-
-
     document.querySelector(`#${selected}`).style.color = 'white';
-    document.querySelector(`#${selected}`).style.fontWeight = 600;
+    document.querySelector(`#${selected}`).style.fontWeight = 500;
+}
 
+function navwithscroll() {
+    const skill_secion = document.querySelector('#skills_section')
+    const home_secion = document.querySelector('#home_section');
+    const work_secion = document.querySelector('#work_section')
+
+    document.addEventListener('scroll',()=>{
+        if (window.scrollY>=0) {
+            makeitbold('about');
+        }
+        if (window.scrollY>=50) {
+            makeitbold('work');
+        }
+        if (window.scrollY>=work_secion.getBoundingClientRect().bottom){
+            makeitbold('more');
+        }
+    })
 }
 
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
 
-    const menu_buttons = document.querySelectorAll('.nav-menu__link');
-    menu_buttons.forEach(menu_btn=>{
+    const menu_buttons = document.querySelectorAll('.nav-menu__item');
+    menu_buttons.forEach(menu_btn=>{    
         menu_btn.addEventListener('click',()=>{
-            makeitbold(menu_btn.id)
+            makeitbold(menu_btn.id.substr(menu_btn.id.length-1))
         })
     })
-
-
-
+    navwithscroll();
 
     const fname = document.querySelector('.logo__fname');
     const lname = document.querySelector('.logo__lname');
@@ -45,14 +55,18 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     const deks_width = 1024;
     if (window.innerWidth>=deks_width) {
-        fname.innerHTML = 'kunal';
-        lname.innerHTML = 'sangtiani';
+        // fname.innerHTML = 'kunal';
+        // lname.innerHTML = 'sangtiani';
         bg.style.backgroundImage = "url('./img/300ppi/dekstop2.png')";
     }
     else {
-        fname.innerHTML = 'ku';
-        lname.innerHTML = 'nal';
+        // fname.innerHTML = 'ku';
+        // lname.innerHTML = 'nal';
         bg.style.backgroundImage = "url('./img/300ppi/phone2.png')";
     }
 })
 
+
+$(window).on("load",function(){
+    $(".loader-container").fadeOut(7000);
+});
