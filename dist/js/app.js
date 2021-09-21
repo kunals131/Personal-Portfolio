502b4f5921bf5d0492f593b9a427993c47d39b3c
kunals131 <86314140+kunals131@users.noreply.github.com>
2021-09-21 18:41:02 +0530

    Sidebar functionality

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,34 @@
+
+const main_page = document.querySelector('main');
+const header = document.querySelector('header');
+
+const texts = [
+    {
+        'usual' : "This project is a full-stack web-app based on django's Rest Framework. User can send, recieve and edit emails, create account, login,logout and whole data is saved with django's default sqlite database.",
+        'shortend' : "Fullstack Application based on django's REST Framework "
+    },
+    {
+        'usual' : "This project is my personal portfolio, High quality ui design without just SASS, and google fonts with functions like theme options,smoothscroll and css animations",
+        'shortend' : "Front-End of Personal Portfolio beautifully designed with SASS"
+    },
+    {
+        'usual' : "Webscrapper is a upgrade website to craigslist, It uses Bs4, Beautiful Soup and djnago to search and show results in a beautiful UI",
+        'shortend' : "Webscrapper built by Beautiful Soup, BS4 and Django"
+    }
+]
+
+function disabledPage(values) {
+    if (values===true) {
+        main_page.style.opacity = 0.6;
+        header.style.opacity = 0.6;
+    }
+    else {
+        main_page.style.opacity = 1;
+        header.style.opacity = 1;
+    }
+}
+
+
 document.addEventListener('scroll', ()=>{
     if (window.scrollY>100) {
         document.querySelector('header').classList.add('dark-bg')
@@ -37,7 +68,22 @@ function navwithscroll() {
     })
 }
 
+function changeText(disp) {
+    const all_texts = document.querySelectorAll('.work__project__text__main');
+    if (disp=='dekstop') {
+        all_texts[0].innerHTML = texts[0].usual;
+        all_texts[1].innerHTML = texts[1].usual;
+        all_texts[2].innerHTML = texts[2].usual;
+    }
+    else {
+        all_texts[0].innerHTML = texts[0].shortend;
+        all_texts[1].innerHTML = texts[1].shortend;
+        all_texts[2].innerHTML = texts[2].shortend;
+    }
+    
 
+    
+}
 
 document.addEventListener('DOMContentLoaded', ()=>{
 
@@ -58,11 +104,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
         // fname.innerHTML = 'kunal';
         // lname.innerHTML = 'sangtiani';
         bg.style.backgroundImage = "url('./img/300ppi/dekstop2.png')";
+        changeText('dekstop');
     }
     else {
         // fname.innerHTML = 'ku';
         // lname.innerHTML = 'nal';
         bg.style.backgroundImage = "url('./img/300ppi/phone2.png')";
+        changeText('phone');
+
     }
 })
 
@@ -70,3 +119,88 @@ document.addEventListener('DOMContentLoaded', ()=>{
 $(window).on("load",function(){
     $(".loader-container").fadeOut(0);
 });
+
+
+
+//sidebar functionality 
+const settings_button = document.querySelector('.button__settings');
+const sidebar2 = document.querySelector('.sidebar');
+settings_button.addEventListener('click', ()=>{
+    if (sidebar2.classList.contains('open')) {
+        sidebar2.classList.remove('open');
+       disabledPage(false);
+    }
+    else {
+        sidebar2.classList.add('open');
+        disabledPage(true);
+    }
+})
+
+
+
+function colourselected(color_switch) {
+    document.querySelector('.logo__box').style.backgroundColor = color_switch;
+    document.querySelector('.home__img').style.boxShadow = `4px 4px 30px -3px ${color_switch}`;
+    document.querySelectorAll('.work__project__button').forEach(b=>{
+        b.style.backgroundColor = color_switch;
+    })
+    document.querySelector('.skills__heading').style.color = color_switch;
+    document.querySelector('.contact__envelope').style.color = color_switch;
+    document.querySelector('.social-icons__link').style.color = color_switch;
+    document.querySelector('.contact__heading__p2').style.color = color_switch;
+    document.querySelector('.button__settings').style.backgroundColor = color_switch;
+    
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    
+})
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const all_colours = document.querySelectorAll('.sidebar__color');
+    all_colours.forEach(each_color=>{
+        each_color.style.backgroundColor = each_color.dataset.color;
+    })
+    all_colours[0].parentElement.classList.add('selected');
+
+    all_colours.forEach(each_color=>{
+        each_color.addEventListener('click',()=>{
+            for(let selection of all_colours) {
+                if (selection.parentElement.classList.contains('selected')) {
+                    selection.parentElement.classList.remove('selected');
+                }
+            }
+            each_color.parentElement.classList.add('selected');
+            colourselected(each_color.dataset.color);
+
+
+        })
+    })
+    document.addEventListener('click', (e)=>{
+        let count_click = 0;
+        if (document.querySelector('.sidebar').classList.contains('open')) {
+            let x = (e.target.classList[0]);
+            // alert(x);
+            if (x=='material-icons') {
+                count_click++;
+                if (count_click==2) {
+                    document.querySelector('.sidebar').classList.remove('open');
+                }
+            }
+            else {
+                let sidebarClasses = ['sidebar', 'sidebar-container', 'sidebar__color', 'sidebar__items' ,'sidebar__sub', 'sidebar__heading'];
+                if (!sidebarClasses.includes(x)) {
+                    document.querySelector('.sidebar').classList.remove('open'); 
+                    disabledPage(false);
+                }
+            }
+        }
+    })
+
+})
+
+
+//Project_responsive 
+
+
